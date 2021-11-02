ba86f5e50793e5f46c1a37410b694cecfdc1ccd1
HyeRyeon Kim <hl0427@naver.com>
2021-11-02 18:12:53 +0900

    #13 [Chore] lint 적용

--- app/src/main/java/com/thequietz/travelog/guide/GuideFragment.kt
+++ app/src/main/java/com/thequietz/travelog/guide/GuideFragment.kt
@@ -1,11 +1,11 @@
 package com.thequietz.travelog.guide
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.thequietz.travelog.R
 import com.thequietz.travelog.databinding.FragmentGuideBinding
 
@@ -17,12 +17,12 @@ class GuideFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_guide, container, false)
         return binding.root
     }
-
 }
