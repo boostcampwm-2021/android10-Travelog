5bd7b1436694578b4eec9f2fe9d3e7d602baf825
HyeRyeon Kim <hl0427@naver.com>
2021-11-03 14:23:20 +0900

    #13 [Chore] MainActivity lint 적용

--- app/src/main/java/com/thequietz/travelog/MainActivity.kt
+++ app/src/main/java/com/thequietz/travelog/MainActivity.kt
@@ -2,19 +2,16 @@ package com.thequietz.travelog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.thequietz.travelog.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    private var _binding: ActivityMainBinding? = null;
+    private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,5 +23,4 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment!!.findNavController()
         binding.bottomNavigation.setupWithNavController(navController)
     }
-
 }
