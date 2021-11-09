c9ebfd0239279c7512f7b2e0de30ee8de9fd28a9
HyeRyeon Kim <hl0427@naver.com>
2021-11-09 13:22:13 +0900

    #33 [Feature] 리사이클러뷰 어댑터 생성 및 적용

---
+++ app/src/main/java/com/thequietz/travelog/schedule/adapter/ScheduleDetailAdapter.kt
@@ -0,0 +1,86 @@
+package com.thequietz.travelog.schedule.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.thequietz.travelog.databinding.ItemRecyclerScheduleDetailAddBinding
+import com.thequietz.travelog.databinding.ItemRecyclerScheduleDetailContentBinding
+import com.thequietz.travelog.databinding.ItemRecyclerScheduleDetailHeaderBinding
+import com.thequietz.travelog.schedule.data.ScheduleDetailItem
+import com.thequietz.travelog.schedule.data.TYPE_CONTENT
+import com.thequietz.travelog.schedule.data.TYPE_HEADER
+
+class ScheduleDetailAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var item = mutableListOf<ScheduleDetailItem>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view: View?
+        return when (viewType) {
+            TYPE_HEADER -> {
+                val binding =
+                    ItemRecyclerScheduleDetailHeaderBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                HeaderViewHolder(binding)
+            }
+            TYPE_CONTENT -> {
+                val binding =
+                    ItemRecyclerScheduleDetailContentBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                ContentViewHolder(binding)
+            }
+            else -> {
+                val binding =
+                    ItemRecyclerScheduleDetailAddBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                AddViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            TYPE_HEADER -> {
+                (holder as HeaderViewHolder).bind(item[position])
+                holder.setIsRecyclable(false)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return item.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return item[position].type
+    }
+
+    inner class HeaderViewHolder(val binding: ItemRecyclerScheduleDetailHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ScheduleDetailItem) {
+            binding.item = item
+        }
+    }
+
+    inner class ContentViewHolder(val binding: ItemRecyclerScheduleDetailContentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ScheduleDetailItem) {
+            binding.item = item
+        }
+    }
+
+    inner class AddViewHolder(val binding: ItemRecyclerScheduleDetailAddBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: ScheduleDetailItem) {
+            binding.item = item
+        }
+    }
+}
--- app/src/main/java/com/thequietz/travelog/schedule/view/ScheduleDetailFragment.kt
+++ app/src/main/java/com/thequietz/travelog/schedule/view/ScheduleDetailFragment.kt
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.thequietz.travelog.R
 import com.thequietz.travelog.databinding.FragmentScheduleDetailBinding
+import com.thequietz.travelog.schedule.adapter.ScheduleDetailAdapter
+import com.thequietz.travelog.schedule.data.ScheduleDetailItem
 import com.thequietz.travelog.schedule.viewmodel.ScheduleDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,6 +19,7 @@ class ScheduleDetailFragment : Fragment() {
     lateinit var binding: FragmentScheduleDetailBinding
     private val layoutId = R.layout.fragment_schedule_detail
     private val viewModel by viewModels<ScheduleDetailViewModel>()
+    lateinit var adapter: ScheduleDetailAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,5 +34,18 @@ class ScheduleDetailFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
+        initRecycler()
+    }
+
+    private fun initRecycler() {
+        adapter = ScheduleDetailAdapter()
+        binding.rvSchedule.adapter = adapter
+        adapter.item.apply {
+            add(ScheduleDetailItem(1))
+            add(ScheduleDetailItem(2))
+            add(ScheduleDetailItem(2))
+            add(ScheduleDetailItem(3))
+        }
+        adapter.notifyDataSetChanged()
     }
 }
