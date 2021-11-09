73be2485e10e1237b150b9de582d8446f126e55e
quavious <leenowon97@gmail.com>
2021-11-09 23:11:53 +0900

    [Feature] 장소 검색 화면 검색어 입력 및 데이터 출력

---
+++ app/src/main/java/com/thequietz/travelog/place/adapter/PlaceSearchAdapter.kt
@@ -0,0 +1,59 @@
+package com.thequietz.travelog.place.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.thequietz.travelog.R
+import com.thequietz.travelog.databinding.ItemRecyclerPlaceSearchBinding
+import com.thequietz.travelog.place.model.PlaceSearchModel
+
+class PlaceSearchAdapter(
+    private val listener: PlaceSearchAdapter.OnItemClickListener
+) :
+    ListAdapter<PlaceSearchModel, PlaceSearchAdapter.ViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemRecyclerPlaceSearchBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_recycler_place_search,
+            parent,
+            false
+        )
+        val viewHolder = ViewHolder(binding)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), position)
+    }
+
+    inner class ViewHolder(private val binding: ItemRecyclerPlaceSearchBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(model: PlaceSearchModel, position: Int) {
+            binding.model = model
+            binding.tvPlaceSearchItem.setOnClickListener {
+                listener.onClickItem(model, position)
+            }
+
+            binding.executePendingBindings()
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<PlaceSearchModel>() {
+            override fun areItemsTheSame(oldItem: PlaceSearchModel, newItem: PlaceSearchModel) =
+                oldItem.placeId == newItem.placeId
+
+            override fun areContentsTheSame(oldItem: PlaceSearchModel, newItem: PlaceSearchModel) =
+                oldItem == newItem
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onClickItem(model: PlaceSearchModel, position: Int): Boolean
+    }
+}
