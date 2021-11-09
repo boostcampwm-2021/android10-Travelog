9ab9c10c86227e26c5cf0507d2b86a50d36ec4fe
Yang Cheol Joo <rlaengks0001@naver.com>
2021-11-09 11:20:24 +0900

    #28 [Feature] 임시 데이터 추가

--- app/src/main/java/com/thequietz/travelog/record/Record.kt
+++ app/src/main/java/com/thequietz/travelog/record/Record.kt
@@ -4,5 +4,5 @@ data class Record(
     val title: String,
     val startDate: String,
     val endDate: String,
-    val images: List<String>
+    val images: List<String>? = null
 )
--- app/src/main/java/com/thequietz/travelog/record/RecordViewModel.kt
+++ app/src/main/java/com/thequietz/travelog/record/RecordViewModel.kt
@@ -16,6 +16,12 @@ class RecordViewModel @Inject constructor() : ViewModel() {
     init {
         viewModelScope.launch {
             // TODO: DB로부터 기록 목록 데이터 로드
+            // 임시 데이터 사용
+            _recordList.value = listOf(
+                Record("제주도여행", "2021.08.30", "2021.09.01", null),
+                Record("경주여행", "2021.10.26", "2021.10.28", null),
+                Record("강릉여행", "2021.12.20", "2021.12.25", null)
+            )
         }
     }
 }
