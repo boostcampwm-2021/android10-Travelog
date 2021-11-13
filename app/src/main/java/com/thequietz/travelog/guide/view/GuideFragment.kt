b7168038f4a4ae930d070d856729748455c1a867
lee <ab72002338@kakao.com>
2021-11-13 22:36:09 +0900

    #53 [Chore] 검색창 한줄로

--- app/src/main/java/com/thequietz/travelog/guide/view/GuideFragment.kt
+++ app/src/main/java/com/thequietz/travelog/guide/view/GuideFragment.kt
@@ -1,11 +1,14 @@
 package com.thequietz.travelog.guide.view
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -18,8 +21,6 @@ import com.thequietz.travelog.guide.adapter.AllPlaceAdapter
 import com.thequietz.travelog.guide.adapter.RecommendPlaceAdapter
 import com.thequietz.travelog.guide.viewmodel.GuideViewModel
 import dagger.hilt.android.AndroidEntryPoint
-
-private const val TAG = "AREA_LIST"
 
 @AndroidEntryPoint
 class GuideFragment : Fragment() {
@@ -61,27 +62,51 @@ class GuideFragment : Fragment() {
         }
         setClickListener()
     }
+
     @SuppressLint("ClickableViewAccessibility")
     private fun setClickListener() {
         binding.etSearch.setOnTouchListener(
-            View.OnTouchListener { p0, p1 ->
+            View.OnTouchListener { view, event ->
                 val DRAWABLE_LEFT = 0
                 val DRAWABLE_TOP = 1
                 val DRAWABLE_RIGHT = 2
                 val DRAWABLE_BOTTOM = 3
-                if (p1?.action == MotionEvent.ACTION_UP) {
-                    if (p1.rawX >= binding.etSearch.right - binding.etSearch.compoundDrawables.get(DRAWABLE_RIGHT).bounds.width()) {
-                        if (binding.etSearch.text.toString() == "") {
-                            Toast.makeText(requireContext(), "검색어를 입력하세요!", Toast.LENGTH_SHORT).show()
-                        } else {
-                            val action = GuideFragmentDirections
-                                .actionGuideFragmentToSpecificGuideFragment(binding.etSearch.text.toString())
-                            findNavController().navigate(action)
-                        }
+
+                if (event?.action == MotionEvent.ACTION_UP) {
+                    if (event.rawX >= binding.etSearch.right - binding.etSearch.compoundDrawables.get(
+                            DRAWABLE_RIGHT
+                        ).bounds.width()
+                    ) {
+                        searchAction()
                     }
                 }
                 false
             }
         )
+        binding.etSearch.setOnKeyListener { _, key, event ->
+            downKeyboard()
+            if ((event.action == KeyEvent.ACTION_DOWN) && (key == KeyEvent.KEYCODE_ENTER)) {
+                searchAction()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    fun searchAction() {
+        if (binding.etSearch.text.toString() == "") {
+            Toast.makeText(requireContext(), "검색어를 입력하세요!", Toast.LENGTH_SHORT).show()
+        } else {
+            val action = GuideFragmentDirections
+                .actionGuideFragmentToSpecificGuideFragment(binding.etSearch.text.toString())
+            findNavController().navigate(action)
+        }
+    }
+
+    fun downKeyboard() {
+        val mInputMethodManager: InputMethodManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        mInputMethodManager.hideSoftInputFromWindow(binding.etSearch.windowToken, 0)
     }
 }
