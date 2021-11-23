05ac13ba0ed488700c1436639f27c7e864c1c8a3
quavious <leenowon97@gmail.com>
2021-11-23 21:42:47 +0900

    #93 [Feature] 여행 일정 열람 & 페이징 적용

--- app/src/main/java/com/thequietz/travelog/confirm/adapter/ConfirmPagerAdapter.kt
+++ app/src/main/java/com/thequietz/travelog/confirm/adapter/ConfirmPagerAdapter.kt
@@ -1,3 +1,60 @@
 package com.thequietz.travelog.confirm.adapter
 
-class ConfirmPagerAdapter
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.thequietz.travelog.R
+import com.thequietz.travelog.databinding.ItemRecyclerConfirmPageBinding
+import com.thequietz.travelog.schedule.model.ScheduleDetailModel
+
+class ConfirmPagerAdapter :
+    ListAdapter<ScheduleDetailModel, ConfirmPagerAdapter.ViewHolder>(ConfirmPagerDiffUtil()) {
+
+    class ViewHolder(val binding: ItemRecyclerConfirmPageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(model: ScheduleDetailModel) {
+            binding.model = model
+            val imageUrl = model.destination.images.firstOrNull()?.imageUrl ?: model.place.thumbnail
+
+            Glide.with(binding.ivConfirmPage)
+                .load(imageUrl)
+                .centerCrop()
+                .into(binding.ivConfirmPage)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemRecyclerConfirmPageBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_recycler_confirm_page,
+            parent,
+            false
+        )
+        val viewHolder = ViewHolder(binding)
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+private class ConfirmPagerDiffUtil : DiffUtil.ItemCallback<ScheduleDetailModel>() {
+    override fun areItemsTheSame(
+        oldItem: ScheduleDetailModel,
+        newItem: ScheduleDetailModel
+    ): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: ScheduleDetailModel,
+        newItem: ScheduleDetailModel
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
--- app/src/main/java/com/thequietz/travelog/confirm/view/ConfirmFragment.kt
+++ app/src/main/java/com/thequietz/travelog/confirm/view/ConfirmFragment.kt
@@ -9,8 +9,10 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager2.widget.ViewPager2
 import com.thequietz.travelog.R
 import com.thequietz.travelog.confirm.adapter.ConfirmDayAdapter
+import com.thequietz.travelog.confirm.adapter.ConfirmPagerAdapter
 import com.thequietz.travelog.confirm.viewmodel.ConfirmViewModel
 import com.thequietz.travelog.databinding.FragmentConfirmBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +25,7 @@ class ConfirmFragment : Fragment() {
 
     private lateinit var _context: Context
     private lateinit var dayAdapter: ConfirmDayAdapter
+    private lateinit var pageAdapter: ConfirmPagerAdapter
 
     private val navArgs: ConfirmFragmentArgs by navArgs()
     private val viewModel: ConfirmViewModel by viewModels()
@@ -47,14 +50,27 @@ class ConfirmFragment : Fragment() {
 
         dayAdapter = ConfirmDayAdapter(object : ConfirmDayAdapter.OnClickListener {
             override fun onClick(index: Int) {
-                TODO("Not yet implemented")
+                pageAdapter.submitList(viewModel.schedules.value?.get("Day ${index + 1}"))
+                binding.vpConfirmPlace.let {
+                    it.post {
+                        it.setCurrentItem(0, true)
+                    }
+                }
             }
         })
+        pageAdapter = ConfirmPagerAdapter()
+
         binding.rvConfirmHeader.adapter = dayAdapter
+        binding.vpConfirmPlace.adapter = pageAdapter
+        binding.vpConfirmPlace.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
         binding.lifecycleOwner = viewLifecycleOwner
 
         viewModel.schedules.observe(viewLifecycleOwner, {
-            dayAdapter.submitList(it.keys.toList())
+            val keys = it.keys.toList()
+
+            dayAdapter.submitList(keys)
+            pageAdapter.submitList(it[keys[0]])
         })
 
         viewModel.getSchedulesByNavArgs(navArgs.schedules)
