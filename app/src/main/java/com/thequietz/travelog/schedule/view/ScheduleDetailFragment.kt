8d1d63a7d8adf962525e479ad4b17b533731ed69
ChoiJaeYeong <chlwodud2@naver.com>
2021-11-23 22:09:40 +0900

    [Fix] Rebase Conflict 해결

--- app/src/main/java/com/thequietz/travelog/schedule/view/ScheduleDetailFragment.kt
+++ app/src/main/java/com/thequietz/travelog/schedule/view/ScheduleDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.thequietz.travelog.schedule.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
